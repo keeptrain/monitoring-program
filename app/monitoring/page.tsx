fba4c5dc49c70f40da3531e662dab1f7d682a462
keeptrain <chefgilang2@gmail.com>
2026-04-18 20:15:27 +0700

    refactor: remove server action on MonitoringPage

--- app/monitoring/page.tsx
+++ app/monitoring/page.tsx
@@ -1,7 +1,5 @@
-import { getPublicAvailableLocations } from "@/features/dashboard/actions/public-available-locations";
 import PublicMonitoringPage from "@/features/monitoring/PublicMonitoringPage";
 
 export default async function MonitoringPage() {
-  await getPublicAvailableLocations();
   return <PublicMonitoringPage />;
 }
