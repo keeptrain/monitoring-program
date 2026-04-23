e5d33b2532761291e383f85353ffc63630cec72a
keeptrain <chefgilang2@gmail.com>
2026-04-23 13:51:57 +0700

    chore(thematic): initialize table constants

---
+++ lib/constants/tables.ts
@@ -0,0 +1,7 @@
+export const TABLES = {
+  BIOFLOC_THEMATIC_PROGRAMS: "biofloc_thematic_programs",
+  PROPOSAL_BIOFLOC_THEMATIC_PROGRAMS: "proposal_biofloc_thematic_programs",
+  LOGS: "monitoring_logs",
+  UPT: "technical_units",
+  USERS: "profiles",
+} as const;
