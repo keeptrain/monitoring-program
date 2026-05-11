e91d926378c8713519ba98dfd5682694b4ef5fd3
keeptrain <chefgilang2@gmail.com>
2026-05-11 19:07:53 +0700

    chore(thematic): add missed api and data files

---
+++ app/(monitoring)/biofloc-thematic/data/page.tsx
@@ -0,0 +1,7 @@
+export default function BioflocThematicDataPage() {
+  return (
+    <div>
+      <h1>Biofloc Thematic Data</h1>
+    </div>
+  );
+}
---
+++ features/thematic/api/getThematicProgram.ts
@@ -0,0 +1,21 @@
+import { queryOptions, useQuery } from "@tanstack/react-query";
+import { getThematicProgramById } from "../actions/biofloc-actions";
+
+export const getThematicProgramQueryKey = (id: string) => [
+  "thematic-program",
+  id,
+];
+
+export const getThematicProgramQueryOptions = (id: string) =>
+  queryOptions({
+    queryKey: getThematicProgramQueryKey(id),
+    queryFn: () => getThematicProgramById(id),
+    staleTime: 1000 * 60 * 3,
+    gcTime: 1000 * 60 * 5,
+  });
+
+export const useGetThematicProgram = (id: string, enabled = true) =>
+  useQuery({
+    ...getThematicProgramQueryOptions(id),
+    enabled,
+  });
